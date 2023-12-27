690062cb05a7aa9b8753c1f2596fddef44b54fb4
JinJinDal <krlawls1@gmail.com>
2023-12-27 13:01:14 +0900

    Update : header script
    스크롤했을때 나왔다가 들어갔다가 추가

--- js/sub-common.js
+++ js/sub-common.js
@@ -1,2 +1,44 @@
+window.onload = function(){
 
-document.querySelector('.top-scroll')
+    const scrlTop = document.getElementById('topBtn'),
+    headerMenu = document.querySelector('header');
+
+    window.addEventListener('scroll',function(){
+        if(this.scrollY < 10){
+            scrlTop.classList.remove('nohide');
+        }else{
+            scrlTop.classList.add('nohide');
+        }
+    });
+
+    let lastScroll = document.documentElement.scrollTop || 0
+    document.addEventListener('scroll',function(){
+        let scrollTop = document.documentElement.scrollTop
+
+        if(scrollTop > lastScroll) {
+            // down
+            // console.log('down');
+            headerMenu.style = `transform: translateY(-100%)`
+        } else {
+            //up
+            // console.log('up');
+            headerMenu.style = `transform: translateY(0)`
+        }
+        lastScroll = scrollTop
+
+    })
+
+
+    scrlTop.onclick = function(){
+        window.scrollTo({
+            top:0,
+            left:0,
+            behavior:"smooth"
+        })
+        console.log('dsa');
+    }
+    console.log(scrollY);
+    
+    
+    console.log('live');
+}
