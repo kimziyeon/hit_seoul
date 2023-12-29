5bb5e36d2ce4c9b2b9767a9bbeec10a43405bdd7
chanwoo717 <chanwoo555sonteen@gmail.com>
2023-12-29 17:09:42 +0900

    Update :inlaw
    drape,brand,symbol.html updated
    fun1,2/inspire1,2/love1,2/symbol1,2,3 redownloaded

--- js/inlaw.js
+++ js/inlaw.js
@@ -1,21 +1,8 @@
 // inlaw.js
 const colorHov = document.querySelectorAll('.outer-box > div'),
     colorBar = document.querySelectorAll('.color_click span');
-    // emblemText = document.querySelectorAll('.emblem-img > figcaption'),
-    // emblemHov = document.querySelectorAll('.emblem-img > img');
 
 function init(){
-
-    // emblemHov.forEach(function(element,key){
-
-    //     element.onmouseover = function(){
-    //         emblemText[key].classList.add('active01');
-    //     }
-
-    //     element.onmouseleave = function(){
-    //         emblemText[key].classList.remove('active01');
-    //     }
-    // })
 
     colorBar.forEach(function(element,key){
         element.onmouseover = function(){
