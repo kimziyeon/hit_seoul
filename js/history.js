64ae8abd79951020a1b94a2b953d26536e97f8cf
JinJinDal <krlawls1@gmail.com>
2024-01-03 18:26:02 +0900

    Update : index,subHistory
    1

--- js/history.js
+++ js/history.js
@@ -1,120 +1,175 @@
 // history.js
-window.onload =function(){
+window.onload = function () {
 
-    function pageHisSeoul(){};
-    function pageHisMayor(){};
-    
-    let pageNum = location.pathname.substr(1);
-    
-            switch(pageNum){
-                case 'info-graphy.html' : pageGraphy();
-                break;
-    
-                case 'info-story.html' : pageStory();
-                break;
-            };
+    function pageHisMayor() {
+        fetch('./data/mayor.json')
+            .then(obj => obj.json())
+            .then(history => {
+                index = history;
+                historyMayor();
+            })
 
+        function historyMayor() {
+            const mayorInfo = document.querySelector('.mayor-sct-bundle');
 
-
-    fetch('./data/mayor.json')
-    .then(obj => obj.json())
-    .then(history =>{
-        index = history;
-        historyMayor();
-    })
-    
-    function historyMayor(){
-        const mayorInfo = document.querySelector('.mayor-sct-bundle');
-    
-        let mayorTag = "";
-        index.forEach(function(key){
-            mayorTag +=`<section class="mayor-sct">
-            <div class="mayor-bundle">
-                <div class="mayor-text-box">
-                    <div class="mayor-info">
-                        <h2 class="mayor-name">${key.name}</h2>
-                        <p>${key.a}</p>
+            let mayorTag = "";
+            index.forEach(function (key) {
+                mayorTag += `<div class="mayor-sct">
+                <div class="mayor-bundle">
+                    <div class="mayor-text-box">
+                        <div class="mayor-info">
+                            <h2 class="mayor-name">${key.name}</h2>
+                            <p>${key.a}</p>
+                        </div>
+                        <div class="mayor-policy">
+                            <b>주요 직책</b>
+                            <p>${key.policy}</p>
+                        </div>
                     </div>
-                    <div class="mayor-policy">
-                        <b>주요 직책</b>
-                        <p>${key.policy}</p>
+                    <div class="mayor-img-box">
+                        <img src="./img/history/page2/${key.img}" alt="">
                     </div>
                 </div>
-                <div class="mayor-img-box">
-                    <img src="./img/history/page2/${key.img}" alt="">
-                </div>
-            </div>
-        </section>`;
-        });
-    
-        mayorInfo.innerHTML = mayorTag;
-    
-    }
-    
-    fetch('./data/history.json')
-    .then(obj => obj.json())
-    .then(history2 =>{
-        indexHistory = history2;
-        historySeoul();
-    })
-    
-    let count = 0;
-    let ancient = historySeoul.filter(obj=>obj.type == '고대');
-    console.log(ancient);
-    
-    function historySeoul(){
-        const historyPage = document.querySelector('.his-sub-bundle'),
-            historyList = document.querySelectorAll('.sub-history-list li'),
-            historyNext = document.querySelector('.sub-history-img-box .next'),
-            historyPrev = document.querySelector('.sub-history-img-box .prev');
+            </div>`;
+            });
 
-        console.log(historyList);
-        historyList.onclick = function(){
-            // this.classList.add('sub-history-list-li');
-            
-            console.log('click');
+            mayorInfo.innerHTML = mayorTag;
+
         }
 
-        indexHistory.forEach(function(key,i){
-            // count = i;
-            
-            historyNext.onclick = function(){
-                const historyTitle = document.querySelector('.sub-history-text-box h3'),
-                    historyText =document.querySelector('.sub-history-text-box p'),
-                    hisImg = document.querySelector('.sub-history-img-box > img');
-                if(count === 9){
-                    return;
-                }
-                count++;
-                hisImg.setAttribute(`src`,`${indexHistory[count].img}`);
-                historyTitle.innerHTML =`<b>${indexHistory[count].name}</b> ${indexHistory[count].subheading}`;
-                historyText.innerText =`${indexHistory[count].text}`;
-                
-            }
-    
-            historyPrev.onclick = function(){
-                const historyTitle = document.querySelector('.sub-history-text-box h3'),
-                historyText =document.querySelector('.sub-history-text-box p'),
-                hisImg = document.querySelector('.sub-history-img-box > img');
+    };
 
-                if(count === 0){
-                    return;
-                }
-                count--;
-                hisImg.setAttribute(`src`,`${indexHistory[count].img}`);
-                historyTitle.innerHTML =`<b>${indexHistory[count].name}</b> ${indexHistory[count].subheading}`;
-                historyText.innerText =`${indexHistory[count].text}`;
-            }
+    function pageHisSeoul() {
+
+
+        // function bindHistoryListClick() {
+            
+        // }
+
+        fetch('./data/history.json')
+        .then(response => response.json())
+        .then(historyData => {
+            indexHistory = historyData;
+
+
+            //여기서 json값을 type별로 묶어준다
+            ancient = indexHistory.filter(obj=>obj.type == '고대');
+            ancientS = Object.keys(ancient).length;
+
+            MiddleAges = indexHistory.filter(obj=>obj.type == '중세');
+            modern = indexHistory.filter(obj=>obj.type == '근대');
+            hyundai = indexHistory.filter(obj=>obj.type == '현대');
+
+            //type별로 묶은걸 활용하기 위해서 하나로 다시 묶는다. 그러면 처음에 0~9까지가 아닌 0 1, 0 1 2, 0 1 2, 0 1로 묶여서 4개의 목록에 넣어서 활용하기 용의하다
+            hisSeoutlS = [ancient, MiddleAges, modern, hyundai];
     
+            historySeoul();
         })
+
+        //li랑 그 안에 이미지를 변경하기 위해서 전부 0으로 설정
+    let count = 0,idx=0;;
+
+    
+    function historySeoul() {
+        const historyPage = document.querySelector('.his-sub-bundle'),
+            historyNext = document.querySelector('.sub-history-img-box .next'),
+            historyPrev = document.querySelector('.sub-history-img-box .prev'),
+            historyList = document.querySelectorAll('.sub-history-list li');
+    
+    
+        historyList.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                historyList.forEach((li) => {
+                    li.classList.remove('active');
+                });
+    
+                item.classList.add('active');
+                idx=index;
+                count=0;
+                historyPrint();
+            });
+        });
+    
+        historyNext.onclick = function () {
+            if(hisSeoutlS[idx].length > count){
+                count++;
+            }
+            historyPrint();
+        }
+    
+        historyPrev.onclick = function () {
+            if(hisSeoutlS[idx].length > count) count--; return;
+            historyPrint();
+        }
+
+        function historyPrint(){
+            const historyTitle = document.querySelector('.sub-history-text-box h3'),
+                historyText = document.querySelector('.sub-history-text-box p'),
+                hisImg = document.querySelector('.sub-history-img-box > img');
+                
+            hisImg.setAttribute('src', hisSeoutlS[idx][count].img);
+            historyTitle.innerHTML = `<b>${hisSeoutlS[idx][count].name}</b> ${hisSeoutlS[idx][count].subheading}`;
+            historyText.innerHTML = `${hisSeoutlS[idx][count].text}`;
+        }
     }
     
+    // $('body').prepend('<header></header>')
+    // $('body').append('<footer></footer>')
+
+    // let nav = function(){
+        
+    // }
+
+    // $('header').load('../../sub-common.html .inner',nav);
+    // $('footer').load('../../sub-common.html .ft-wrap',nav);
+    // $('.subcommon-container').load('../../sub-common.html .banner-img',nav);
+
+
+
+
+
+
+
+
+
+
+        
+
+    };
+
+
+
+    let pageNum = location.pathname.substr(1);
+
+    switch (pageNum) {
+        case 'his-seoul.html': pageHisSeoul();
+            break;
+
+        case 'his-mayor.html': pageHisMayor();
+            break;
+    };
+
+    $('body').prepend('<header></header>')
+    $('body').append('<footer></footer>')
+
+    let nav = function(){
+        
+    }
+
+    $('header').load('../../sub-common.html .inner',nav);
+    $('footer').load('../../sub-common.html .ft-wrap',nav);
+    $('.subcommon-container').load('../../sub-common.html .banner-img',nav);
+
+
+
+
+
 }
-//저게 1 2 첫번째 목록에서 이동하고 두번째는 3 4 세번째는 5 6 네번째는 7 8 그러면 두번째 목록부터 n+2로 하면 
+//저게 1 2 첫번째 목록에서 이동하고 두번째는 3 4 세번째는 5 6 네번째는 7 8 그러면 두번째 목록부터 n+2로 하면
 // 1 2
 // 3 4
 // 5 6
-// 7 8 
+// 7 8
 
 
 //next랑 prev를 누르면 밑에 제목과 글 교체
@@ -125,7 +180,7 @@ window.onload =function(){
 
 //    /his-seoul.html
 
-/* 
+/*
 let pageName = location.pathname,
     pageIdx = pageName.indexOf('.'),
     pageStr = pageName.substring(1,pageIdx);
@@ -135,10 +190,10 @@ let pageName = location.pathname,
         case 'his-mayor': 함수실행; break;
     }
  */
-    // let param = new URLSearchParams(location.search);
-    // console.log(
-    //     param.get('code')
-    // )
+// let param = new URLSearchParams(location.search);
+// console.log(
+//     param.get('code')
+// )
 
 
 
--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
-// index.js
-window.onload =function(){
+window.onload = function () {
+    var swiper = new Swiper(".mySwiper", {
+        watchSlidesProgress: true,
+        slidesPerView: 3,
+    });
 
-    const oneScroll = document.querySelector('main'),
-        container = document.querySelectorAll('section'),
-        lastPage = container.length - 1;
-        let page = 0;
+    window.addEventListener('scroll', function () {
+        // console.log(window.scrollY);
 
-        window.addEventListener('wheel',(event)=>{
-            
-        })
+    });
+
+    //header 메뉴 색상 변경
+    const hdMenu = document.querySelectorAll('.container'),
+          main = document.querySelector('main'),
+        hdMenuA = document.querySelectorAll('.header-menu-item a');
+
+    let inter,num=0;
+    window.onscroll = function(){
+        
+        if(hdMenu[0].getBoundingClientRect().top < 0){
+            // hdMenuA.classList.add('active');
+        }else{
+            // hdMenuA.classList.remove('active');
+        }
+        // hdMenu.forEach(function(menu,key){
+        //     if(menu.getBoundingClientRect().top < window.innerHeight* 0.5){
+        //         menu.classList.add('active')
+        //     }
+        // })
+
+        clearTimeout(inter)
+        inter = setTimeout(()=>{
+            num++;
+            main.style=`position:fixed;width:100%;transform:translateY(-${window.innerHeight * num}px);transition:1s`;
+        },300)
+        
+    }
+
 }
